05630a997613168e7eeec2fa4b045452dd68e488
APerezBanks <88187553+APerezBanks@users.noreply.github.com>
2022-03-15 12:17:09 +0000

    conected to front and back

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 require("./db/connection");
 const express = require("express");
+const cors = require("cors");
 const movieRouter = require("./movie/movieRoutes");
 const userRouter = require("./user/userRoutes");
 const app = express();
 const port = 5001;
 // add things to the application, the ability to work with json data, pass json data convetr to js objects
 app.use(express.json());
+
+// is  used to connect the back with front I think. the position is really important in need to be above the routers.
+app.use(cors());
+
 // grab everything thats been added  to movie router and Userrouter
 app.use(movieRouter);
 app.use(userRouter);
@@ -14,6 +19,10 @@ app.use(userRouter);
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
+
+
+
+
 
 
 // prior to do any request( imsomia ) lines 1-12 have been run
